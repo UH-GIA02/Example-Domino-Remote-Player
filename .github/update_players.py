4311e0ba68a95c2ff909728b7b16547c021df274
Leonardo16AM <leo16am@gmail.com>
2023-11-19 00:39:08 -0500

    Changed update_players.py

--- .github/update_players.py
+++ .github/update_players.py
@@ -1,5 +1,6 @@
 import json
 import os
+import base64
 from github import Github
 
 def update_players_json(user, repo_link):
@@ -7,26 +8,33 @@ def update_players_json(user, repo_link):
     g = Github(token)
     repo = g.get_repo("UH-GIA02/Domino-Tournament")
     
-    contents = repo.get_contents("src/data/players.json")
-    players_data = json.loads()
+    try:
+        contents = repo.get_contents("src/data/players.json")
+        players_data = json.loads(base64.b64decode(contents.content).decode('utf-8'))
 
-    found = False
-    for player in players_data:
-        if player['github_user'] == user:
-            player['repo'] = repo_link  
-            found = True
-            break
-    if not found:
-        new_player = {
-            "github_user": user,
-            "games_played": [],
-            "repo": repo_link
-        }
-        players_data.append(new_player)
+        found = False
+        for player in players_data:
+            if player['github_user'] == user:
+                player['repo'] = repo_link  
+                found = True
+                break
 
-    updated_content = json.dumps(players_data, indent=4).encode()
+        if not found:
+            new_player = {
+                "github_user": user,
+                "games_played": [],
+                "repo": repo_link
+            }
+            players_data.append(new_player)
 
-    repo.update_file(contents.path, "Update players.json", updated_content, contents.sha)
+        updated_content = base64.b64encode(json.dumps(players_data, indent=4).encode()).decode().replace('\n', '')
+
+        repo.update_file(contents.path, "Update players.json", updated_content, contents.sha)
+
+        print("Archivo players.json actualizado con éxito.")
+    
+    except Exception as e:
+        print(f"Error al actualizar players.json: {e}")
 
 if __name__ == "__main__":
     github_user = os.getenv('PR_AUTHOR')
